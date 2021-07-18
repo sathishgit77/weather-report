6fb1cb7e56b94190e916226900f88e00fd2f7c03
Sathish Kumar <sathishkumar.marimuthu@borngroup.com>
2021-07-18 17:24:54 +0530

    Keroku app deployment changes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require("./utils/forecast")
 const geocode = require("./utils/geocode")
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, "../public")
 const templatePath = path.join(__dirname, "../templates/views")
@@ -85,6 +86,6 @@ app.get("*", (req, res) => {
     })
 })
 
-app.listen("3000", () => {
-    console.log("Server is up")
+app.listen(port, () => {
+    console.log("Server is up at " + port)
 })
